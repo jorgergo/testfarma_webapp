914616c7d9b7b12871a97cad04d541a1f7941a8d
Ricardo Adolfo González Terán <ricardo_glez_14@icloud.com>
2023-05-29 15:20:39 -0600

    Added Recomendations

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,12 +5,16 @@ from django.shortcuts import redirect
 from login.views import *
 from login.urls import *
 
+
 # Create your views here.
 
 
 def home(request):
     return render(request, "home.html")
 
+def recomendations(request, user_id):
+    
+    return render(request, "recomendations.html")
 
 def appointments(request):
     return render(request, "appointments/appointments.html")
