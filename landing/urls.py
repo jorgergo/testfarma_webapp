f1c8aa7012debe1fa2c5f36cc1008f12b30b788e
Marino <35277555+Mahrex308@users.noreply.github.com>
2023-06-06 02:32:05 -0600

    .

--- landing/urls.py
+++ landing/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    path("testfarma", views.testfarma, name="testfarma"),
     path("services", views.services, name="services"),
     path("termsandconditions", views.termsandconditions, name="termsandconditions"),
 ]
--- landing/views.py
+++ landing/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def testfarma(request):
+    return render(request, "testfarma.html")
+
 def services(request):
     return render(request, "services.html")
 
