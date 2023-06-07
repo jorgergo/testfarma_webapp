f6e50730fa758194255e6779408a1c2558abce02
Marino <35277555+Mahrex308@users.noreply.github.com>
2023-06-07 02:21:21 -0600

    .

--- landing/views.py
+++ landing/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def testfarma(request):
-    return render(request, "testfarma.html")
+    return render(request, "index.html")
 
 def services(request):
     return render(request, "services.html")
