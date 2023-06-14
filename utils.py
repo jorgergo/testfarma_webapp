b7ca0440f920ead5d868a4c6b5862889f9ff1539
Ricardo Adolfo González Terán <ricardo_glez_14@icloud.com>
2023-06-13 19:45:42 -0600

    Minor changes

--- utils.py
+++ utils.py
@@ -1,8 +1,25 @@
 from pymongo import MongoClient
 
 def get_db_handle(db_name):
-    client = MongoClient("mongodb://localhost:27017")
+    
+    client = MongoClient("mongodb+srv://TESTFARMATST:3czzys7FtN6XysKr@testfarmatst.crr3uq1.mongodb.net/?retryWrites=true&w=majority")
+    
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+        
+        db = client[db_name]
+        
+        return db
+        
+    except Exception as e:
+        
+        print(e)
+        
+        return None
 
-    db = client[db_name]
+    
 
-    return db
+
+
+
